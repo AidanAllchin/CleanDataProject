7dc1b02571546b645c6f57572985cb4953dc57c0
AidanAllchin <aidan@allchin.com>
2023-07-08 10:47:12 -0700

    Code done and commented (save for any potential errors). Also pushing processed "tidy.csv"

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-library(dplyr)
+library(plyr)
 
 # Import for fread()
 library(data.table)
@@ -23,3 +23,38 @@ yTest <- fread(file = "./data/UCI HAR Dataset/test/y_test.txt")
 subjectTrain <- fread(file = "./data/UCI HAR Dataset/train/subject_train.txt")
 xTrain <- fread(file = "./data/UCI HAR Dataset/train/X_train.txt")
 yTrain <- fread(file = "./data/UCI HAR Dataset/train/y_train.txt")
+
+features <- fread(file = "./data/UCI HAR Dataset/features.txt")
+activities <- fread(file = "./data/UCI HAR Dataset/activity_labels.txt")
+
+# PART 1
+# Mash it all together to get one data table
+fullMessy <- as.data.frame(cbind(rbind(subjectTest, subjectTrain),
+                   rbind(yTest, yTrain), rbind(xTest, xTrain)))
+
+# PART 4
+# Appropriately name columns, first two manually and all others from features
+colnames(fullMessy)[1:2] <- c("Subject", "Activity")
+endIndex <- ncol(fullMessy)
+colnames(fullMessy)[3:endIndex] <- as.data.frame(features)[, 2]
+rm(endIndex)
+
+# PART 2
+# Remove parts of the data table that aren't mean, std, activity, or subject
+fullMessy <- fullMessy[, grepl("mean()|std()|Activity|Subject",
+                               colnames(fullMessy)) & 
+                           !grepl("meanFreq", colnames(fullMessy))]
+
+# PART 3
+# Labeling data with activity names
+fullMessy$Activity <- factor(fullMessy$Activity,
+                             levels = as.data.frame(activities)[, 1], 
+                             labels = as.data.frame(activities)[, 2])
+
+tidy <- ddply(fullMessy, c("Subject", "Activity"),
+              .fun = function(x) {colMeans(x[, -c(1:2)])})
+
+# PART 5
+# Dump a summary to the console and then create a csv with the result
+str(tidy)
+write.csv2(tidy, "./data/tidy.txt", row.names = FALSE)
